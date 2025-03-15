b97ac3cc2683ccaf854e767a1faf4659a170b083
mmsstudent2021 <mmsstudent2021@gmail.com>
2025-03-15 14:30:27 +0630

    js day 1

---
+++ 3-learn-javascript/1-overview-day1.js
@@ -0,0 +1,52 @@
+// Data type 
+// string 
+let name = "su su"
+
+// number 
+let age = 5
+let weight = 20.8
+let tem = -20
+
+// boolean => true , false 
+
+// array 
+let fruits = ["apple","orange","mango"]
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// object 
+let myself = {
+    name : "su su",
+    age : 20
+}
+console.log(myself);
+console.log(myself["name"]);
+console.log(myself["age"]);
+console.log(myself.name);
+console.log(myself.age);
+
+
+// operators 
+// arithmetic 
+let x = 5
+let y = 10
+console.log(5 + 10);
+console.log(10 - 5);
+
+// assign 
+let aa = "aa"
+let z = 5
+z += 10
+console.log(z);
+
+// comparison 
+console.log(10 > 5);
+console.log(10 < 5);
+
+// string 
+console.log(5 + 5);
+console.log("su" + "su");
+console.log("su" + 5);
+console.log(5 + "su");
